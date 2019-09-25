c5eccaf5fa4944137b3584c602623766cb75e89c
石益然 <1101419059@qq.com>
2019-09-25 12:16:20 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Hello></Hello>
+    <Fade></Fade>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import Fade from './components/fade'
+export default {
+  name: 'App',
+  components: {
+    Hello,
+    Fade
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
